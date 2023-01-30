27233da45fce1f9e09d58f0ecc0c9c1428470942
cyasing <59028917+cyasing@users.noreply.github.com>
2023-01-30 17:17:25 +0530

    Added in basic power-to-current function.

--- liionpack/protocols.py
+++ liionpack/protocols.py
@@ -1,3 +1,4 @@
+
 #
 # Experimental protocol
 #
@@ -7,30 +8,46 @@ import numpy as np
 
 def generate_protocol_from_experiment(experiment, flatten=True):
     """
-
     Args:
         experiment (pybamm.Experiment):
             The experiment to generate the protocol from.
         flatten (bool):
             Default is True: return all steps in one list otherwise return a
             list of lists for each operating command.
-
     Returns:
         list:
             a sequence of terminal currents to apply at each timestep
-
     """
     protocol = []
     for i, op in enumerate(experiment.operating_conditions):
         proto = []
         t = op["time"]
         dt = op["period"]
-        if t % dt != 0:
-            raise ValueError("Time must be an integer multiple of the period")
+        # if t % dt != 0:
+        #     raise ValueError("Time must be an integer multiple of the period")
+
         typ = op["type"]
-        if typ not in ["current"]:
-            raise ValueError("Only constant current operations are supported")
-        else:
+
+        if typ in ["power"]:
+            if typ == "power":
+                if "Power input [W]" in op.keys():
+                    P = op["Power input [W]"]
+                    V = 3.9
+                    I = P/V
+                    proto.extend([I] * int(t / dt))
+                    if i == 0:
+                        # Include initial state when not drive cycle, first op
+                        proto = [proto[0]] + proto
+                elif "dc_data" in op.keys():
+                    dc_data = op["dc_data"]
+                    V = 3.9
+                    dc_data_I = P/V
+                    proto.extend(dc_data_I[:, 1].tolist())
+                    
+        # if typ not in ["current"]:
+        #     raise ValueError("Only constant current operations are supported")
+        # else:
+        if typ in ["current"]:
             if typ == "current":
                 if "Current input [A]" in op.keys():
                     I = op["Current input [A]"]
