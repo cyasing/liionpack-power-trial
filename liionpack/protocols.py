62ff84aa29f66ba41423033f1f69146caac2cc4a
cyasing <59028917+cyasing@users.noreply.github.com>
2023-01-30 21:37:58 +0530

    Added power trial 1

--- liionpack/protocols.py
+++ liionpack/protocols.py
@@ -20,6 +20,7 @@ def generate_protocol_from_experiment(experiment, flatten=True):
     """
     protocol = []
     for i, op in enumerate(experiment.operating_conditions):
+        op["Terminal voltage [V]"][0] = 3 * 2 # initilising with "nominal voltage"
         proto = []
         t = op["time"]
         dt = op["period"]
@@ -27,22 +28,24 @@ def generate_protocol_from_experiment(experiment, flatten=True):
         #     raise ValueError("Time must be an integer multiple of the period")
 
         typ = op["type"]
-
         if typ in ["power"]:
             if typ == "power":
                 if "Power input [W]" in op.keys():
                     P = op["Power input [W]"]
-                    V = 3.9
-                    I = P/V
+                    #V = 3.9
+                    if t == 0:
+                        P = V * I
+                        V[0] = P[0]/I[0]
+                    for i in t[1:end]:
+                        I[i] = P[i]/V[i-1]
                     proto.extend([I] * int(t / dt))
                     if i == 0:
                         # Include initial state when not drive cycle, first op
                         proto = [proto[0]] + proto
-                elif "dc_data" in op.keys():
-                    dc_data = op["dc_data"]
-                    V = 3.9
-                    dc_data_I = P/V
-                    proto.extend(dc_data_I[:, 1].tolist())
+                # elif "dc_data" in op.keys():
+                #     dc_data = op["dc_data"]
+                    
+                #     proto.extend(dc_data_I[:, 1].tolist())
                     
         # if typ not in ["current"]:
         #     raise ValueError("Only constant current operations are supported")
