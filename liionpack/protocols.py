e41a89ed48229235a8e37bec20f757030b104240
cyasing <59028917+cyasing@users.noreply.github.com>
2023-01-31 19:23:27 +0530

    Power Trial 3

--- liionpack/protocols.py
+++ liionpack/protocols.py
@@ -22,6 +22,7 @@ def generate_protocol_from_experiment(experiment, flatten=True):
     for i, op in enumerate(experiment.operating_conditions):
         # op["Terminal voltage [V]"][0] = 3 * 2 # initilising with "nominal voltage"
         proto = []
+
         t = op["time"]
         dt = op["period"]
         # if t % dt != 0:
@@ -31,7 +32,11 @@ def generate_protocol_from_experiment(experiment, flatten=True):
         if typ in ["power"]:
             if typ == "power":
                 if "Power input [W]" in op.keys():
+                    
                     P = op["Power input [W]"]
+                    I = op["Current input [A]"]
+                    V = op["Terminal voltage [V]"]
+
                     #V = 3.9
                     if t == 0:
                         P = V * I
@@ -68,3 +73,63 @@ def generate_protocol_from_experiment(experiment, flatten=True):
             protocol.append(proto)
 
     return protocol
+'''
+        def external_circuit_function(variables):
+            I = variables["Current [A]"]
+            V = variables["Terminal voltage [V]"]
+            return V + I - pybamm.FunctionParameter("Function", {"Time [s]": pybamm.t})
+
+    def get_fundamental_variables(self):
+        param = self.param
+        # Current is a variable
+        i_var = pybamm.Variable("Current density variable")
+        if self.control in ["algebraic", "differential without max"]:
+            i_cell = i_var
+        elif self.control == "differential with max":
+            i_input = (
+                pybamm.FunctionParameter(
+                    "CCCV current function [A]",
+                    {"Time [s]": pybamm.t * param.timescale},
+                )
+                / param.I_typ
+                * pybamm.sign(param.I_typ)
+            )
+            i_cell = pybamm.maximum(i_var, i_input)
+
+        # Update derived variables
+        I = i_cell * abs(param.I_typ)
+        i_cell_dim = I / (param.n_electrodes_parallel * param.A_cc)
+
+        variables = {
+            "Current density variable": i_var,
+            "Total current density": i_cell,
+            "Total current density [A.m-2]": i_cell_dim,
+            "Current [A]": I,
+            "C-rate": I / param.Q,
+        }
+
+        # Add discharge capacity variable
+        variables.update(super().get_fundamental_variables())
+
+        return variables
+
+class PowerFunctionControl(FunctionControl):
+    """External circuit with power control."""
+
+    def __init__(self, param, options, control="algebraic"):
+        super().__init__(param, self.constant_power, options, control=control)
+
+    def constant_power(self, variables):
+        I = variables["Current [A]"]
+        V = variables["Terminal voltage [V]"]
+        P = V * I
+        P_applied = pybamm.FunctionParameter(
+            "Power function [W]", {"Time [s]": pybamm.t * self.param.timescale}
+        )
+        if self.control == "algebraic":
+            return P - P_applied
+        else:
+            # Multiply by the time scale so that the overshoot only lasts a few seconds
+            K_P = 0.01 * self.param.timescale
+            return -K_P * (P - P_applied)
+'''
